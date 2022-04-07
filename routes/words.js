9a2369e4db89fdc0bbd8d07b232005666c71770b
Silver1793 <git>
2022-04-06 21:26:36 -0400

    adjusted

--- routes/words.js
+++ routes/words.js
@@ -2,14 +2,12 @@ const express = require("express"),
   router = express.Router(),
   mongoose = require("mongoose");
 
-const list = ["mmmmmm", "sssssrld", "aaaa", "qqqq", "uuuuu"];
+const list = ["music", "teleport", "water", "reading", "books"];
 
 function chooseRandom() {
   return list[Math.floor(Math.random() * list.length)];
 }
-function trial() {
-  console.log("HE:RERE");
-}
+
 const c = { chooseRandom: chooseRandom };
 
 module.exports = c;
